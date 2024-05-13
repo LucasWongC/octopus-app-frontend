bb7d651057f6a039e61f97587214be98adc6b64f
LucasWongC <wm111898@gmail.com>
2024-05-12 19:01:02 -0700

    css bugfix

--- src/components/layout/Dropdown.tsx
+++ src/components/layout/Dropdown.tsx
@@ -55,7 +55,7 @@ function Dropdown() {
               className="w-full flex justify-between items-center"
             >
               <p className="block px-4 py-2">Documentation</p>
-              <IoDocumentTextOutline className="w-4 h-4 mr-4" />
+              <IoDocumentTextOutline className="w-5 h-5 mr-4" />
             </Link>
           </li>
           <li className="flex gap-2 justify-between items-center hover:bg-darkgrey-400">
@@ -64,7 +64,7 @@ function Dropdown() {
               className="w-full flex justify-between items-center"
             >
               <p className="block px-4 py-2">Explorer</p>
-              <BiSearchAlt className="w-4 h-4 mr-4" />
+              <BiSearchAlt className="w-5 h-5 mr-4" />
             </Link>
           </li>
         </ul>
